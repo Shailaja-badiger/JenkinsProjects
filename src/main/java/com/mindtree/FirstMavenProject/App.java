98d44c9c8dc3ae6b3da44783274cd2c76734f7f6
Ubuntu <shailajabadiger@shailajabadiger.ab0q2x2jhgtepamlroet4hstmf.gx.internal.cloudapp.net>
2020-11-02 10:12:51 +0000

    Pushing maven project to git

---
+++ src/main/java/com/mindtree/FirstMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.mindtree.FirstMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My first maven" );
+    }
+}
